76dd250afd0e79d34f4728b2b446283414e3dc54
Collin Smith <collinsmith@csupomona.edu>
2014-11-17 15:04:08 -0800

    Added simple evaluation function to parse a string a return a value based on the passed token type

--- src/com/gmail/collinsmith70/toycompiler/lexer/Evaluator.java
+++ src/com/gmail/collinsmith70/toycompiler/lexer/Evaluator.java
@@ -4,4 +4,16 @@ public class Evaluator {
 	private Evaluator() {
 		//...
 	}
+
+	public static final Object evaluate(TokenTypes type, String lexeme) {
+		switch (type) {
+			case _characterliteral:	return lexeme.charAt(0);
+			case _booleanliteral:	return Boolean.valueOf(lexeme);
+			case _integerliteral:	return Integer.valueOf(lexeme);
+			case _doubleliteral:	return Double.valueOf(lexeme);
+			case _stringliteral:	return lexeme;
+			case _nullliteral:	return null;
+			default:			return null;
+		}
+	}
 }
