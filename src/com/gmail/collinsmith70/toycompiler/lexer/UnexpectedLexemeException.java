242da8d873d8fb0950843b1931026436e3aab35d
Collin Smith <collinsmith@csupomona.edu>
2014-11-19 19:48:51 -0800

    Documented UnexpectedLexemeException and removed default constructor (doesn't provide needed information to the user)

--- src/com/gmail/collinsmith70/toycompiler/lexer/UnexpectedLexemeException.java
+++ src/com/gmail/collinsmith70/toycompiler/lexer/UnexpectedLexemeException.java
@@ -1,11 +1,20 @@
 package com.gmail.collinsmith70.toycompiler.lexer;
 
+/**
+ * This class represents an exception that is thrown whenever a {@link Scanner}
+ * detects an unsupported lexeme (symbol or word).
+ *
+ * @author Collin Smith <strong>collinsmith70@gmail.com</strong>
+ */
 public class UnexpectedLexemeException extends RuntimeException {
-	public UnexpectedLexemeException() {
-		//...
-	}
-
-	public UnexpectedLexemeException(int line, String string) {
-		super(String.format("Unexpected lexeme scanned on line %d, \"%s\"", line, string));
+	/**
+	 * Constructs an {@code UnexpectedLexemeException} which will output the
+	 * given line and lexeme.
+	 *
+	 * @param line line number within the file that this lexeme exists
+	 * @param lexeme lexeme that is not supported
+	 */
+	public UnexpectedLexemeException(int line, String lexeme) {
+		super(String.format("Unexpected lexeme scanned on line %d, \"%s\"", line, lexeme));
 	}
 }
