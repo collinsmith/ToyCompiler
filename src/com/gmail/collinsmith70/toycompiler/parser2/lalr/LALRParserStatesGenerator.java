81f99124f1c0a59cc1516e838c7dd24f6433d7a2
Collin Smith <collinsmith@csupomona.edu>
2014-11-30 01:32:48 -0800

    Final commit before I retire for the night. Nothing major added, just some preliminary stuff for generating LALR closure items

--- src/com/gmail/collinsmith70/toycompiler/parser2/lalr/LALRParserStatesGenerator.java
+++ src/com/gmail/collinsmith70/toycompiler/parser2/lalr/LALRParserStatesGenerator.java
@@ -6,10 +6,16 @@ import com.gmail.collinsmith70.toycompiler.parser2.NonterminalSymbol;
 import com.gmail.collinsmith70.toycompiler.parser2.ProductionRuleInstance;
 import com.gmail.collinsmith70.toycompiler.parser2.State;
 import com.gmail.collinsmith70.toycompiler.parser2.Symbol;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class LALRParserStatesGenerator extends AbstractParserStatesGenerator {
 	public LALRParserStatesGenerator() {
@@ -40,6 +46,71 @@ public class LALRParserStatesGenerator extends AbstractParserStatesGenerator {
 					}
 				}
 			}
+
+			return;
+		}
+
+		Set<ProductionRuleInstance> closureItems = new HashSet<>();
+		kernelItems.stream()
+			.forEach(kernelItem -> closeOver(kernelItem, g, kernelItems, closureItems, productionRuleInstances));
+
+		List<Symbol> viablePrefixes;
+		if (parent != null) {
+			viablePrefixes = new LinkedList<>(parent.getViablePrefixes());
+			viablePrefixes.add(symbol);
+		} else {
+			viablePrefixes = new LinkedList<>();
+		}
+
+		int stateId = states.size();
+		State state = new State(
+			stateId,
+			parent,
+			ImmutableList.copyOf(viablePrefixes),
+			kernelItems,
+			ImmutableSet.copyOf(closureItems)
+		);
+
+		states.put(kernelItems, state);
+		if (parent != null) {
+			parent.putTransition(symbol, state);
+		}
+
+		Deque<ProductionRuleInstance> remainingProductions = new LinkedBlockingDeque<>(kernelItems);
+		remainingProductions.addAll(closureItems);
+
+		ProductionRuleInstance p;
+		Set<ProductionRuleInstance> reductions = new HashSet();
+		while (!remainingProductions.isEmpty()) {
+			p = remainingProductions.pollFirst();
+			if (p.hasNext()) {
+				Symbol lookahead = p.peekNextSymbol();
+				Set<ProductionRuleInstance> productionsWithSameLookahead = new HashSet<>();
+				remainingProductions.stream()
+					.filter(sibling -> (sibling.hasNext() && Objects.equals(sibling.peekNextSymbol(), lookahead)))
+					.forEach(sibling -> {
+						productionsWithSameLookahead.add(sibling);
+					});
+
+				remainingProductions.removeAll(productionsWithSameLookahead);
+				productionsWithSameLookahead.add(p);
+
+				Set<ProductionRuleInstance> nextKernelItems = new HashSet<>();
+				productionsWithSameLookahead.stream()
+					.forEach((productionWithSameNextSymbol) -> nextKernelItems.add(productionWithSameNextSymbol.next()));
+
+				State.Metadata childMetadata = state.getChildMetadata(lookahead, ImmutableSet.copyOf(nextKernelItems));
+				deque.offerLast(childMetadata);
+			} else {
+				reductions.add(p);
+			}
+		}
+
+		if (1 < reductions.size()) {
+			//numWithReduceReduce++;
+			g.getLogger().warning(String.format("State %d has %d reduce productions:", stateId, reductions.size()));
+			reductions.stream()
+				.forEach(reduce -> g.getLogger().warning(String.format("\t%s", reduce.toString(g.getSymbolsTable().inverse()))));
 		}
 	}
 
@@ -51,6 +122,18 @@ public class LALRParserStatesGenerator extends AbstractParserStatesGenerator {
 		Set<ProductionRuleInstance> closureItems,
 		Map<NonterminalSymbol, ImmutableSet<ProductionRuleInstance>> productionRuleInstances
 	) {
+		if (!p.hasNext()) {
+			return;
+		}
 
+		Symbol lookahead = p.peekNextSymbol();
+		if (lookahead instanceof NonterminalSymbol) {
+			for (ProductionRuleInstance closureItem : productionRuleInstances.get(lookahead)) {
+				if (!(kernelItems.contains(closureItem) || closureItems.contains(closureItem))) {
+					closureItems.add(closureItem);
+					//closeOver(closureItem, g, kernelItems, closureItems, productionRuleInstances);
+				}
+			}
+		}
 	}
 }
