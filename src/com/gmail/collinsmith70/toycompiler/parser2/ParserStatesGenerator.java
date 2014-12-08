f617aad7ad89f8b1dae4b9278c5dacffd763ad3a
Collin Smith <collinsmith@csupomona.edu>
2014-12-08 14:35:34 -0800

    Removed parser2 package as it should no longer be needed

--- src/com/gmail/collinsmith70/toycompiler/parser2/ParserStatesGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.gmail.collinsmith70.toycompiler.parser2;
-
-import java.util.Map;
-import java.util.Set;
-
-public interface ParserStatesGenerator {
-	Map<Set<ProductionRuleInstance>, State> generateParserTables(Grammar g);
-}
--- src/com/gmail/collinsmith70/toycompiler/parser2/slr/SLRParserTables.java
+++
@@ -1,7 +0,0 @@
-package com.gmail.collinsmith70.toycompiler.parser2.slr;
-
-public class SLRParserTables {
-	public SLRParserTables() {
-		//...
-	}
-}
